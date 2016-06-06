d7f31adbca53873a935f5d970d444ea536746d10
sithi1990 <sithi1990@live.com>
2016-06-06 18:55:34 +0530

    Initial Commit

---
+++ app/models/user_coordinate.js
@@ -0,0 +1,12 @@
+// get an instance of mongoose and mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('UserCoordinate', new Schema({
+	name: String,
+	email: String,
+    userName: String,
+	longitude: String,
+	latitude: String
+}));
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'secret': 'vappsecret',
+    'database': 'mongodb://vuser:vuser123@jello.modulusmongo.net:27017/eJ8agego'
+
+};
